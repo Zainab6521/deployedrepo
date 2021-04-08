bdc3a6dcd9594f874a5bdd9fdc9d5c62e1a6debb
zainab raza <zk.652162@gmail.com>
2021-04-08 12:42:28 +0500

    modifying arrays

---
+++ js/new.js
@@ -0,0 +1,32 @@
+document.write("<h1>heading</h1>")
+alert("Ruko zara sabar karo.");
+console.log("hello!");
+
+let array = [];
+for (let i = 0; i <= 15; i++) {
+    array.push(i)
+}
+document.write(array);
+// let array = [];
+// for (let i = 0; i < 15; i * 2) {
+//     array.push(i);
+// }
+// document.write(array);
+
+// array.forEach(myFunction)
+// function myFunction(item, index, array) {
+//     array[index] = item * 10;
+// }
+// document.write(array);
+
+// var newarray = array.map(myFunction)
+// function myFunction(i) {
+//   return i * 10;
+// }
+
+// array.forEach(item => {
+//     // document.write(item.array+2)
+//     // array = item.array + 2
+//     item + 2;
+// });
+// document.write(array);
