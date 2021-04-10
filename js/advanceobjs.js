8a16dc2fc2b683af158b56e6561f8d2b8b434749
zainab raza <zk.652162@gmail.com>
2021-04-10 21:24:09 +0500

    new concepts of JS

---
+++ js/advanceobjs.js
@@ -0,0 +1,143 @@
+const robot = {
+    model: 'B-4MI',
+    mobile: true,
+    greeting() {
+        console.log(`I'm model 
+      ${this.model}, how may I be of service?`);
+    }
+  }
+  
+  const massProdRobot = (model, mobile) => {
+    return {
+      model,
+      mobile,
+      greeting() {
+        console.log(`I'm model ${this.model}, how may I be of service?`);
+      }
+    }
+  }
+  
+  const shinyNewRobot = massProdRobot('TrayHax', true)
+  
+  const chargingStation = {
+    _name: 'Electrons-R-Us',
+    _robotCapacity: 120,
+    _active: true,
+    _chargingRooms: ['Low N Slow', 'Middle of the Road', 'In and Output'],
+  
+    set robotCapacity(newCapacity) {
+      if (typeof newCapacity === 'number') {
+        this._robotCapacity = newCapacity;
+      } else {
+        console.log(`Change ${newCapacity} to a number.`)
+      }
+    },
+    get robotCapacity() {
+      return this._robotCapacity;
+    }
+  }
+
+  //ANOTHER ONE
+  const robot = {
+    model: '1E78V2',
+    energyLevel: 100,
+    provideInfo() { 
+      return `I am ${this.model} and my current energy level is ${this.energyLevel}.`
+    }
+  };
+  
+  console.log(robot.provideInfo());
+
+  //GETTER method
+  const robot = {
+    _model: '1E78V2',
+    _energyLevel: 100,
+    get energyLevel(){
+      if(typeof this._energyLevel === 'number') {
+        return 'My current energy level is ' + this._energyLevel
+      } else {
+        return "System malfunction: cannot retrieve energy level"
+      }
+    }
+  };
+  
+  console.log(robot.energyLevel);
+  
+//Setter method
+
+const robot = {
+    _model: '1E78V2',
+    _energyLevel: 100,
+    _numOfSensors: 15,
+    get numOfSensors(){
+      if(typeof this._numOfSensors === 'number'){
+        return this._numOfSensors;
+      } else {
+        return 'Sensors are currently down.'
+      }
+    },
+    set numOfSensors(num) {
+      if (typeof num === 'number' && num >= 0){
+        this._numOfSensors = num;
+      } else {
+        console.log('Pass in a number that is greater than or equal to 0')
+      }   
+    } 
+  };
+  
+  robot.numOfSensors = 100;
+  console.log(robot.numOfSensors);
+
+  //factory function
+  let robotFactory = (model, mobile) => {
+    return {
+      model :model,
+      mobile: mobile,
+      beep(){
+        console.log('Beep Boop');
+      }
+    }
+  }
+  const tinCan = robotFactory('P-500',true);
+  tinCan.beep();
+
+  //Destructed assignment
+  const robot = {
+    model: '1E78V2',
+    energyLevel: 100,
+    functionality: {
+      beep() {
+        console.log('Beep Boop');
+      },
+      fireLaser() {
+        console.log('Pew Pew');
+      },
+    }
+  };
+  const { functionality } = robot;
+  functionality.beep();
+  
+  
+  //Built IN objects Method
+
+  const robot = {
+	model: 'SAL-1000',
+  mobile: true,
+  sentient: false,
+  armor: 'Steel-plated',
+  energyLevel: 75
+};
+
+// What is missing in the following method call?
+const robotKeys = Object.keys(robot);
+
+console.log(robotKeys);
+
+// Declare robotEntries below this line:
+const robotEntries = Object.entries(robot)
+console.log(robotEntries);
+
+// Declare newRobot below this line and use object.assign property:
+const newRobot = Object.assign({laserBlaster: true, voiceRecognition: true}, robot);
+
+console.log(newRobot);
