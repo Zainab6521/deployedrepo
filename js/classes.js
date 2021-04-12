d50013ff9d8f1d5783ef51946bf79379874c1eb5
zainab raza <zk.652162@gmail.com>
2021-04-12 13:22:08 +0500

    complete basics

--- js/classes.js
+++ js/classes.js
@@ -21,4 +21,199 @@ class Dog {
   halley.incrementBehavior(); // Add one to behavior
   console.log(halley.name); // Print name value to console
   console.log(halley.behavior); // Print behavior value to console
+
+
+//Instance of classes
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+const surgeonRomero = new Surgeon('Francisco Romero','Cardiovascular');
+const surgeonJackson = new Surgeon('Ruth Jackson','Orthopedics');
+
+//Methods
+class Surgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
   
+  get name() {
+    return this._name;
+  }
+  
+  get department() {
+    return this._department;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
+const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
+
+//Inheritance
+
+class HospitalEmployee{
+  constructor(name){
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  get name(){
+    return this._name;
+  }
+  get remainingVacationDays(){
+    return this._remainingVacationDays;
+  }
+  takeVacationDays(daysOff){
+    this._remainingVacationDays -= daysOff;
+  }
+}
+class Doctor{
+  constructor(name,insurance){
+    this._name = name;
+    this._remainingVacationDays = 20;
+    this._insurance= insurance;
+  }
+takeVacationDays(){
+
+  }
+}
+class Nurse{
+  constructor(name,certification){
+    this._name = name;
+    this._remainingVacationDays = 20;
+    this._certifications= certification;
+  }
+takeVacationDays(){
+
+  }
+addCertification(){
+
+}
+
+}
+
+// properties inheritance
+
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+class Nurse extends HospitalEmployee{
+  constructor(name,certifications){
+    super(name);
+    this._certifications = certifications;
+  }
+}
+const nurseOlynyk = new Nurse('Olynyk',['Trauma', 'Pediatrics']);
+
+//  inheritance 
+
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  } 
+  get certifications(){
+    return this._certifications;
+  }
+  addCertification(newCertification){
+    this._certifications.push(newCertification);
+  }
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
+console.log(nurseOlynyk.addCertification('Genetics'));
+console.log(nurseOlynyk.certifications);
+
+//static method:
+
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+
+static generatePassword(){
+  return Math.floor(Math.random() * 10000);
+}
+}
+
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  } 
+  
+  get certifications() {
+    return this._certifications;
+  }
+  
+  addCertification(newCertification) {
+    this.certifications.push(newCertification);
+  }
+}
+//we cant call static method on instance
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
